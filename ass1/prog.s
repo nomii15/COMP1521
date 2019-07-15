3d8bccc5174a473d0bead7e40bb719091cf126a1
nad <52521750+nomii15@users.noreply.github.com>
2019-07-16 04:06:58 +1000

    Add files via upload

--- ass1/prog.s
+++ ass1/prog.s
@@ -8,293 +8,218 @@
 # - newBoard (byte[][]): next board state
 ## Global data
 	.data
-one:    .byte 1
-zeero:  .byte 0
-msg1:	.asciiz "# Iterations: "
-msg2:	.asciiz "=== After iteration "
-msg3:   .asciiz " ==="
-msg4:   .asciiz "."
-msg5:   .asciiz "#"
-eol:	.asciiz "\n"
+msg1:   .asciiz "# Iterations: "
+msg2:   .asciiz "=== After iteration "
+msg3:   .asciiz " ===\n"
+eol:    .asciiz "\n"
+hash:   .asciiz "#"
+dot:    .asciiz "."
 
-## Provides:
+one:        .byte 1
+zeero:      .byte 0
+
+maxiter:    .word 1
+nn:         .word 0
+
+ra_main:    .space 4
+ra_CBAS:    .space 4
+ra_neigh:   .space 4
+ra_decide:  .space 4
+ra_preCBAS: .space 4
+
+	.text
 	.globl	main
 	.globl	decideCell
 	.globl	neighbours
-	.globl	copyBackAndShow
-
-
-########################################################################
-# .TEXT <main>
-	.text
+	
+###############################MAIN###################################
 main:
-
-# Frame:	...
-# Uses:		...
-# Clobbers:	...
-
-# Locals:	...
-
-# Structure:
-#	main
-#	-> [prologue]
-#	-> ...
-#	-> [epilogue]
-
-# Code:
-
-    sw	$fp, -4($sp)				# push $fp onto stack
-	la	$fp, -4($sp)				# set up $fp for this function
-	sw	$ra, -4($fp)				# save return address
-	addi	$sp, $sp, -8			# reset $sp to last pushed item	
-
-    la 	$a0, msg1
-    li 	$v0, 4
-    syscall             			#printf("Iterations: ")
-    
-    li	$v0, 5
-	syscall			    			# scanf("%d", into $v0)
-	move  $s0, $v0    				#$s0 = maxiters
+	sw   $ra, ra_main
 	
-	li  $s1, 1						#$s1 = n = 1
-	li  $s2, 0						#$s2 = i = 0
-	li  $s3, 0						#$s3 = j = 0
-	lw  $s4, N						#$s4 = N
-	li  $s5, 0						#$s5 = nn = 0
-	#li  $s6, -1						#$s6 = x = -1
-	#li  $s7, -1						#$s7 = y = -1
+	la   $a0, msg1		
+	li   $v0, 4
+	syscall                     #printf("# Iterations: ");
 
-    li $s1, 1           			#initialise n = 1
-top_for_loop:
-    bgt $s1, $s0 end_top 			#break outer loop if n > maxiters ($s1 > $s0)    
-    li $s2, 0           			#initialise i = 0
-
-mid_for_loop:
-    bge $s2, $s4 end_mid 			#break mid loop if i >= N    
-    li $s3, 0           			#initialise j = 0
-
-low_for_loop:
-    bge $s3, $s4 end_mid 			#break mid loop if j >= N
-    move $a0, $s2
-    move $a1, $s3
-    jal neighbours
-    nop
-    add $s5, $s5, $v0                   #copy value of nn from $v0(found in neighbours) into $s5
-return_neigh:								    	
-	mul $t0, $s2, $s4  				#$t0 = current row * total number of cols ((i) * N)
-	add $t0, $t0, $s3				#total number of bytes traversed (iN) + j
+	li   $v0, 5				
+	syscall
+    move  $s3, $v0              # maxiter
+    
+    lw   $s4, N 				#N
 	
-	lb  $t1, board($t0)				#loading byte at address MemAdd[board[0][0] + offset]
-	lb  $t8, one                   #let $t8 = 1(byte)
-    beq  $t1, $t8, make_one         #if $t1(byte) = $t8(byte) 
-    li   $t9, 0                     #set $t9 = 0 (int)
-    j end_make_one
-make_one:
-    li $t9, 1                       #set $t9 = 1 (int)
-end_make_one:
-    move $a0, $t9 
-    move $a1, $s5
-	jal decideCell					#will return with either a 0 or 1. Now need to insert return value in correct position
-    nop
-return_decide:
-    move $t2, $v0							
-	mul  $t1, $s2, $s4				#$t1 = current row * total number of cols ((i) * N)
-	add  $t1, $t1, $s3				#total number of bytes traversed (iN) + j	
-	sb  $t2, newBoard($t1)			#set the byte held in $t2 from decide_cell, into MemAddress(newBoard[i][j] + offset($t1))
+    li   $s0, 1					#n = 1
+top_loop:							
+	bgt  $s0, $s3, end_top          #n > maxiter
+    li  $s1, 0
+mid_loop:
+	bge  $s1, $s4, end_mid_loop     #i >= N
+	li   $s2, 0                     #j = 0
+
+low_loop:
+	bge  $s2, $s4, end_low_loop     #j >= N
+
+	jal  neighbours
+	jal  decideCell 			
+
+	addi $s5, $s5, 1                #matrix_counter++;
+	addi $s2, $s2, 1                #j++;
+	j  low_loop
+end_low_loop:
+	addi $s1, $s1, 1                #i++;
+	j  mid_loop
+end_mid_loop:	
+	jal  printf
+	jal  CopyBackAndShow
 	
-    addi $s3, $s3, 1    			#increment j by 1
-    j low_for_loop
-end_low:
-    addi $s2, $s2, 1    			#increment i by 1
-    li  $s3, 0						#reset j = 0
-    j mid_for_loop
-end_mid:
-    la $a0, msg2
-    li $v0, 4
-    syscall             			#printf("=== After iteration ")
-    
-    move $a0, $s1
-    li $v0, 1
-    syscall             			#printf("%d") ---- current value of n
-    
-    la $a0, msg3
-    li $v0, 4
-    syscall             			#printf(" ===")
-    
-    la $a0, eol
-    li $v0, 4
-    syscall             			#printf("\n")
-    
-    j copyBackAndShow   			#jump to label CopyBackandShow
-return_CPAS:   
-    addi $s1, $s1, 1    			#increment n by 1
-    li  $s2, 0						#reset i = 0
-    li  $s3, 0						#reset j = 0
-    j top_for_loop
+	addi $s0, $s0, 1                #n++;
+	li   $s5, 0                     #matrix_counter = 0;
+	j  top_loop
 end_top:
-    
-    li $v0, 0           			#return 0    
-
-main__post:
-    lw  $ra, -4($fp)
-    la  $sp, 4($fp)
-    lw  $fp, ($fp)
-	jr	$ra
-
-
-
-
-
-
+	lw   $ra, ra_main
+	jr   $ra
+#################################END MAIN#############################
+################################DecideCell############################
 decideCell:
-    sw	$fp, -4($sp)				# push $fp onto stack
-	la	$fp, -4($sp)				# set up $fp for this function
-	sw	$ra, -4($fp)				# save return address
-	addi	$sp, $sp, -8			# reset $sp to last pushed item
-    #lb   $t8, one                   #let $t8 = 1(byte)
-    #beq  $t1, $t8, make_one         #if $t1(byte) = $t8(byte) 
-    #li   $t9, 0                     #set $t9 = 0 (int) 
-#if_statements:
-	bne  $a0, 1, big_else_if		#old = 1?
-	beq  $a1, 2, char_one			#nn = 2
-	beq  $a1, 3, char_one			#nn = 3
-	
-char_zero:	
-	lb  $t2, zeero    				#$t2(ret) = 0
-	j end_char_zero
-	#jr $ra
-	#nop
-char_one:
-	lb  $t2, one
-	#j return_decide					#$t2(ret) = 1
-	#jr $ra
-	#nop
-big_else_if:						#else if (nn == 3)
-	beq  $a1, 3, char_one			#nn = 3
-	j char_zero
-end_char_zero:
-#make_one:
-    #li $t9, 1                       #set $t9 = 1 (int)
-    #j if_statements 
-    move $v0, $t2
+    sw  $ra, ra_decide
     
-    lw  $ra, -4($fp)
-    la  $sp, 4($fp)
-    lw  $fp, ($fp)
-	jr	$ra
-
-
-
-
-
-
+    lb  $t2, board($s5)
+	lb  $t0, one
+    lw   $t1, nn
+    
+    bne  $t2, $t0, big_else_if
+    beq  $t1, 2, char_one
+    beq  $t1, 3, char_one
+char_zero:    
+    lb   $t0, zeero
+	sb   $t0, newBoard($s5)
+	j finish
+char_one:    
+    lb   $t0, one
+	sb   $t0, newBoard($s5)
+	j finish
+big_else_if:
+    beq  $t1, 3, char_one
+    j char_zero  
+finish:
+    lw   $ra, ra_decide
+	jr   $ra
+################################END DecideCell##############################
+###################################Neigbours################################
 neighbours:
-    sw	$fp, -4($sp)				# push $fp onto stack
-	la	$fp, -4($sp)				# set up $fp for this function
-	sw	$ra, -4($fp)				# save return address
-	addi	$sp, $sp, -8			# reset $sp to last pushed item
+    sw   $ra, ra_neigh
+
+    li   $t0, 0             #t0 = 0
+    sw   $t0, nn            #nn = t0 = 0
+
+    lw   $t5, N             #N
+    addi $t5, $t5, -1       #N - 1
+
+    li   $t7, -1                    #x = -1
+x_loop:
+	bgt  $t7, 1, end_x_loop         #x > 1?
+    li   $t6, -1                    #reset y = -1
+y_loop:
+	bgt  $t6, 1, end_y_loop         #y > 1?
+
+	li   $t0, 0
+	add  $t0, $s1, $t7              #i + x
+	blt  $t0, $zero, iterate        #i + x < 0
+	bgt  $t0, $t5, iterate          #i + x > N - 1
+
+	li   $t0, 0
+	add  $t0, $s2, $t6 
+	blt  $t0, $zero, iterate 	    #j + y < 0
+	bgt  $t0, $t5, iterate          #j + y > N - 1
+
+	li   $t0, 0
+	bne  $t7, $t0, board_check	    # x == 0?
+	bne  $t6, $t0, board_check      # y == 0?
+	j  iterate
 	
-    li $t2, 0                       #temp nn = 0	
-	li $t0, -1                      #x = -1
-
-neigh_top_loop:
-    bgt $t0, 1 neigh_top_end        #if x > 1, break    
-    li $t1, -1                      #y = -1
-
-neigh_bottom_loop:
-    bgt $t1, 1 neigh_bottom_end     #if y > 1, break
-    
-	add $t5, $a0, $t0				#$t5 = x + i
-	add $t6, $a1, $t1				#$t6 = y + j
-	addi $t7, $s4, -1				#$t7 = N - 1
-
-	blt $t5, $zero, iterate			#i + x < 0 goto next iteration
-	bgt $t5, $t7, iterate			#i + x > N - 1 goto next iteration
-	blt $t6, $zero, iterate			#j + y < 0 goto next iteration
-	bgt $t6, $t7, iterate			#j + y > N - 1 goto next iteration
-	beq $t0, $zero, check_y			#x == 0, check y
-	j end_check_y	
-
-check_y:
-	beq $t1, $zero, iterate			#y == 0 goto next iteration
-
-end_check_y:	
-	mul  $t4, $s4, $t5				#$t4 = current row * Number of columns - ((i + x) * N)
-	add  $t4, $t4, $t6				#$t4 = $t4 + (j + y)
+board_check:
+	lw   $t1, N
+	mul  $t9, $t7, $t1  	#(i + x) * N
+	add  $t9, $t9, $t6  	#((i + x) * N) + (j + y)
+	add  $t3, $s5, $t9  	#((i + x) * N) + (j + y) + matrix counter
 	
-	lb  $t3, board($t4)				#loading the byte at MemAddress board + offset ($t4)
-	lb  $t4, one  					#loading one(1) into $t4
+	lb   $t4, board($t3)    #board[i+x][j+y]
+	lb   $t0, one           #t0 = 1(byte)
 	
-	beq $t3, $t4, nn_plus			#jump to increment nn if condition passes
-	j iterate
+	bne  $t0, $t4, iterate  #board[i+x][j+y] == 1(byte)?
 	
-nn_plus:
-	addi $t2, $t2, 1
+    lw   $t8, nn            #t8 = nn
+    addi $t8, $t8, 1        #t8++
+    sw   $t8, nn            #nn = t8
+	j  iterate
 
 iterate:
-    addi $t1, $t1, 1                #increment y by 1
-	j neigh_bottom_loop
-	
-neigh_bottom_end:    
-    addi $t0, $t0, 1                #increment x by 1
-    li  $t1, -1						#reset y = -1
-    j neigh_top_loop
+	addi $t6, $t6, 1        #y++
+	j  y_loop
+
+end_y_loop:
+	addi $t7, $t7, 1        #x++
+	j 	 x_loop
+end_x_loop:
+	lw   $ra, ra_neigh
+	jr   $ra
+###############################END Neigbours################################
+##################################printf####################################
+printf:
+    sw  $ra, ra_preCBAS
     
-neigh_top_end:
-    move $v0, $t2
-	#j return_neigh
-    lw  $ra, -4($fp)
-    la  $sp, 4($fp)
-    lw  $fp, ($fp)
-	jr	$ra
-
-
-
-
-
-
-copyBackAndShow:	
-	li $t0, 0                   	#initialise i($t0) to 0
-
-loop_out:
-    bge $t0, $s4 end_loop_out    	#break loop if i >= N    
-    li $t1, 0                   	#initialise j($t1) to 0
+    la   $a0, msg2
+	li   $v0, 4
+	syscall             #printf("=== After iteration ")
+	
+	move $a0, $s0
+	li   $v0, 1
+	syscall             #printf("%d")
+	
+	la   $a0, msg3
+	li   $v0, 4
+	syscall             #printf(" ===\n")
     
-loop_in:
-    bge $t1, $s4 end_loop_in     	#break loop if j >= N
+    lw  $ra, ra_preCBAS
+    jr  $ra
+###################################END_PRINTF################################
+######################################CBAS###################################
+CopyBackAndShow:
+    sw   $ra, ra_CBAS
+
+	li   $s5, 0                 #matrix counter = 0
+    li   $s1, 0                 #i = 0  
+i_loop:
+	bge  $s1, $s4, end_i_loop
+	li   $s2, 0                 #j = 0
+j_loop:
+	bge  $s2, $s4, end_j_loop
+
+	lb   $t1, newBoard($s5)     #t1 = newboard(cell)
+	sb   $t1, board($s5)        #board(cell) = t1
+	lb   $t0, zeero             #t0 = one(byte)
+	beq  $t1, $t0, put_dot      #t0 = t1?
+put_hash:
+	lb   $a0, hash
+	li   $v0, 11
+	syscall                     #printf("#");
 	
-	mul $t3, $s4, $t0				#$t3 = current row * total number of cols ((i) * N)
-	add $t3, $t3, $t1				#$t3 = itself + current column
-
-	lb  $t4, newBoard($t3)			#load into $t4 the byte at location newboard[0][0] + offset
-	sb  $t4, board($t3)				#save that byte into the location board[0][0] + offset
+	j  next_col 
+put_dot:
+	lb   $a0, dot
+	li   $v0, 11
+	syscall                     #printf(".");        
+next_col:
+	addi $s5, $s5, 1            #matrix_counter++
+	addi $s2, $s2, 1            #j++;
 	
-	lb  $t5, zeero					#load into $t5 a byte 0, to compare in the if conditions
-	bne $t4, $t5, else
+	j  j_loop
+end_j_loop:
+	la   $a0, eol
+	li   $v0, 4
+	syscall                     #printf("\n");
 	
-	la  $a0, msg4
-	li  $v0, 4						#printf(".")
-	syscall
-
-	j end_else	
-
-else:
-	la  $a0, msg5
-	li  $v0, 4						#printf("#")
-	syscall
-
-end_else:
-    addi $t1, $t1, 1            	#increment j by 1
-    j loop_in
-
-end_loop_in:
-    la $a0, eol             
-    li $v0, 4
-    syscall                     	#printf("\n")
-    
-    addi $t0, $t0, 1            	#increment i by 1
-    li  $t1, 0						#reset j = 0
-    j loop_out
-
-end_loop_out:	
-	j return_CPAS
+	addi $s1, $s1, 1            #i++;
+	j 	 i_loop
+end_i_loop:
+	lw   $ra, ra_CBAS
+	jr   $ra
+###################################END_CBAS################################
